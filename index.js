1cf229de584b6cd6f58a6e4ac227a18ba6f97e4b
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-01-06 22:45:42 +0100

    remove array member

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //like Object.freeze
-
 let notDeleteProperty = new Proxy(target, {
     deleteProperty(trapTarget, key) {
 
@@ -10,3 +9,18 @@ let notDeleteProperty = new Proxy(target, {
         }
     }
 });
+
+
+//create a new array method that removes a member
+function removeArrayMember(array) {
+	if (!Array.isArray(array)) {
+		throw new TypeError('Expected an array');
+	}
+
+	return new Proxy(array, {
+		set: (target, index, member) => {
+			index = target.indexOf(member);
+			return index > -1 ? target.splice(index, 1) : 'there is no such member';
+		}
+	});
+}
